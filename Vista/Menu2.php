d107f147169ecd41df0b9a85a78fda782685eddf
cebastian_16 <sebastianponton14@gmail.com>
2023-04-25 12:54:54 -0500

    2

--- Vista/Menu2.php
+++ Vista/Menu2.php
@@ -1,10 +1,11 @@
-<a href="generar.php">
+<a >
   <?php
   if ($_SESSION['sedeStock'] == "Servired") {
     echo "<img style='height: 68px; width: 200px;' src='http://localhost/Arqueos/Vista/img/servired.png'>";
   } else {
     echo "<img style='height: 68px; width: 200px;' src='http://localhost/Arqueos/Vista/img/multired.png'>";
   }
+   echo $_SESSION['userLogin'] 
   ?>
 
 </a>
